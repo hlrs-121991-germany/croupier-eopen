1b25b1de0188452e06e372a715e802371e5707c5
Dineshkumar RAJAGOPAL <dinesh121991@gmail.com>
2020-07-27 22:53:50 +0200

    Input Output deployment

--- croupier_plugin/tests/blueprints/blueprint_noa/scripts/download_input.sh
+++ croupier_plugin/tests/blueprints/blueprint_noa/scripts/download_input.sh
@@ -3,7 +3,11 @@
 ftp_username=$1
 ftp_password=$2
 dest_location=$3
+src_location=$4
 
-echo $ftp_username
-echo $ftp_password
+echo $ftp_username &> ./ftp_download_input_username.txt
+echo $ftp_password &> ./ftp_download_input_password.txt
+echo $dest_location &> ./ftp_download_input_dest_location.txt
+echo $src_location &> ./ftp_download_input_src_location.txt
+# Replace SCP with FTP client libraries
 scp -r test-eopen:~/EOPEN-Test/ $dest_location                                           
--- croupier_plugin/tests/blueprints/blueprint_noa/scripts/upload_output.sh
+++ croupier_plugin/tests/blueprints/blueprint_noa/scripts/upload_output.sh
@@ -2,10 +2,12 @@
 
 ftp_username=$1
 ftp_password=$2
-file_location=$3
+upload_location=$4
+download_location=$3
 
-echo $ftp_username
-echo $ftp_password
-
-
-scp $file_location test-eopen:~/EOPEN-Test/
+echo $ftp_username &> ./ftp_upload_output_username.txt
+echo $ftp_password &> ./ftp_upload_output_password.txt
+echo $upload_location &> ./ftp_upload_output_upload_location.txt
+echo $download_location &> ./ftp_upload_output_download_location.txt
+# Replace SCP with FTP client libraries
+scp $upload_location test-eopen:~/EOPEN-Test/
